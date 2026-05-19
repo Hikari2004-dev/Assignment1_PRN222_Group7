33efadf13a59ab892d76df741b53f09b01366659
Pipoboyy205 <khanhkhoi08@gmail.com>
2026-05-19 22:02:46 +0700

    First push project

---
+++ Assignment1_PRN222_Group7/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Assignment1_PRN222_Group7.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Assignment1_PRN222_Group7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Assignment1_PRN222_Group7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assignment1_PRN222_Group7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
